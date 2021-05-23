92a85420261d01b22bf5466b63b98c4282817648
aronCiucu <ardrone007@gmail.com>
2021-05-23 15:21:01 +0300

    Prototype Hornet support

---
+++ src/main/java/main/Waypoints/PlanesCommands/F18.java
@@ -0,0 +1,136 @@
+package main.Waypoints.PlanesCommands;
+
+import main.models.Coordinate;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class F18 {
+    public static JSONArray getCommands(ArrayList<Coordinate> coords){
+        /*
+            AMPCD stuff, device 37
+            PB 18 - 3028
+            PB 2  - 3012
+            PB 10 - 3020
+            PB 12 - 3022
+            PB 5  - 3015
+
+            UFC stuff, device 25
+            Select 1 - 3010
+            1 - 3019
+            2 (north) - 3020
+            3 - 3021
+            4 - 3022
+            5 - 3023
+            6 (east) - 3024
+            7 - 3025
+            8 - 3026
+            9 - 3027
+            0 - 3018
+            ENT - 3029
+            CLR - 3028
+         */
+
+        JSONArray commandArray = new JSONArray();
+
+        //enter the SUPT menu
+        commandArray.put(new JSONObject().put("device", "37").put("code", "3028").put("delay", "20").put("activate", "1"));
+        commandArray.put(new JSONObject().put("device", "37").put("code", "3028").put("delay", "20").put("activate", "1"));
+        //select HSD
+        commandArray.put(new JSONObject().put("device", "37").put("code", "3012").put("delay", "20").put("activate", "1"));
+        //select DATA
+        commandArray.put(new JSONObject().put("device", "37").put("code", "3020").put("delay", "20").put("activate", "1"));
+
+        for (Coordinate coordinate:coords) {
+            //increment steerpoint
+            commandArray.put(new JSONObject().put("device", "37").put("code", "3022").put("delay", "20").put("activate", "1"));
+            //press UFC
+            commandArray.put(new JSONObject().put("device", "37").put("code", "3015").put("delay", "20").put("activate", "1"));
+            // press position 1
+            commandArray.put(new JSONObject().put("device", "25").put("code", "3010").put("delay", "20").put("activate", "1"));
+            //press CLR
+            commandArray.put(new JSONObject().put("device", "25").put("code", "3028").put("delay", "20").put("activate", "1"));
+            //press N north
+            commandArray.put(new JSONObject().put("device", "25").put("code", "3020").put("delay", "20").put("activate", "1"));
+            //start entering latitude
+            for(char digit:coordinate.getLatitude().toCharArray()){
+                switch (digit){
+                    case '1':
+                        commandArray.put(new JSONObject().put("device", "25").put("code", "3019").put("delay", "20").put("activate", "1"));
+                        break;
+                    case '2':
+                        commandArray.put(new JSONObject().put("device", "25").put("code", "3020").put("delay", "20").put("activate", "1"));
+                        break;
+                    case '3':
+                        commandArray.put(new JSONObject().put("device", "25").put("code", "3021").put("delay", "20").put("activate", "1"));
+                        break;
+                    case '4':
+                        commandArray.put(new JSONObject().put("device", "25").put("code", "3022").put("delay", "20").put("activate", "1"));
+                        break;
+                    case '5':
+                        commandArray.put(new JSONObject().put("device", "25").put("code", "3023").put("delay", "20").put("activate", "1"));
+                        break;
+                    case '6':
+                        commandArray.put(new JSONObject().put("device", "25").put("code", "3024").put("delay", "20").put("activate", "1"));
+                        break;
+                    case '7':
+                        commandArray.put(new JSONObject().put("device", "25").put("code", "3025").put("delay", "20").put("activate", "1"));
+                        break;
+                    case '8':
+                        commandArray.put(new JSONObject().put("device", "25").put("code", "3026").put("delay", "20").put("activate", "1"));
+                        break;
+                    case '9':
+                        commandArray.put(new JSONObject().put("device", "25").put("code", "3027").put("delay", "20").put("activate", "1"));
+                        break;
+                    case '0':
+                        commandArray.put(new JSONObject().put("device", "25").put("code", "3018").put("delay", "20").put("activate", "1"));
+                        break;
+                }
+            }
+            //press enter
+            commandArray.put(new JSONObject().put("device", "25").put("code", "3029").put("delay", "20").put("activate", "1"));
+            //press E east
+            commandArray.put(new JSONObject().put("device", "25").put("code", "3024").put("delay", "20").put("activate", "1"));
+            //start entering longitude
+            for(char digit:coordinate.getLongitude().toCharArray()){
+                switch (digit){
+                    case '1':
+                        commandArray.put(new JSONObject().put("device", "25").put("code", "3019").put("delay", "20").put("activate", "1"));
+                        break;
+                    case '2':
+                        commandArray.put(new JSONObject().put("device", "25").put("code", "3020").put("delay", "20").put("activate", "1"));
+                        break;
+                    case '3':
+                        commandArray.put(new JSONObject().put("device", "25").put("code", "3021").put("delay", "20").put("activate", "1"));
+                        break;
+                    case '4':
+                        commandArray.put(new JSONObject().put("device", "25").put("code", "3022").put("delay", "20").put("activate", "1"));
+                        break;
+                    case '5':
+                        commandArray.put(new JSONObject().put("device", "25").put("code", "3023").put("delay", "20").put("activate", "1"));
+                        break;
+                    case '6':
+                        commandArray.put(new JSONObject().put("device", "25").put("code", "3024").put("delay", "20").put("activate", "1"));
+                        break;
+                    case '7':
+                        commandArray.put(new JSONObject().put("device", "25").put("code", "3025").put("delay", "20").put("activate", "1"));
+                        break;
+                    case '8':
+                        commandArray.put(new JSONObject().put("device", "25").put("code", "3026").put("delay", "20").put("activate", "1"));
+                        break;
+                    case '9':
+                        commandArray.put(new JSONObject().put("device", "25").put("code", "3027").put("delay", "20").put("activate", "1"));
+                        break;
+                    case '0':
+                        commandArray.put(new JSONObject().put("device", "25").put("code", "3018").put("delay", "20").put("activate", "1"));
+                        break;
+                }
+            }
+            //press enter
+            commandArray.put(new JSONObject().put("device", "25").put("code", "3029").put("delay", "20").put("activate", "1"));
+        }
+
+        return commandArray;
+    }
+}
--- src/main/java/main/Waypoints/WaypointManager.java
+++ src/main/java/main/Waypoints/WaypointManager.java
@@ -3,6 +3,7 @@ package main.Waypoints;
 import main.DCSconnection.PortListenerThread;
 import main.DCSconnection.PortSender;
 import main.Waypoints.PlanesCommands.F16;
+import main.Waypoints.PlanesCommands.F18;
 import main.models.Coordinate;
 
 import java.math.BigDecimal;
@@ -14,20 +15,52 @@ public class WaypointManager {
 
     public static void send(ArrayList<Coordinate> decimalCoords){
         String model = PortListenerThread.getPlaneModel();
-        ArrayList<Coordinate> coords = convertCoords(decimalCoords);
 
         if(model != null && !decimalCoords.isEmpty()){
             if(model.equals("F-16C_50")){
+                ArrayList<Coordinate> coords = convertViperCoords(decimalCoords);
                 String dataToSend = F16.getCommands(coords).toString();
                 PortSender.send(dataToSend);
-            } else if(model.equals("FA-188")){
-
+            } else if(model.equals("FA-18C_hornet")){
+                ArrayList<Coordinate> coords = convertHornetCoords(decimalCoords);
+                String dataToSend = F18.getCommands(coords).toString();
+                PortSender.send(dataToSend);
             }
         }
 
     }
 
-    private static ArrayList<Coordinate> convertCoords(ArrayList<Coordinate> decimalCoords){
+    private static ArrayList<Coordinate> convertHornetCoords(ArrayList<Coordinate> decimalCoords){
+        ArrayList<Coordinate> finalCoords = new ArrayList<>();
+
+        for(Coordinate coordinate: decimalCoords){
+            String latDegrees, latMinutes, latDecimalMinute, longDegrees, longMinutes, longDecimalMinute;
+            // deal with the lat
+            var latBD = new BigDecimal(coordinate.getLatitude());
+            latDegrees = latBD.toBigInteger().toString();
+            var latDegreesDecimalBD = latBD.subtract(new BigDecimal(latDegrees));
+            var latMinutesBD = latDegreesDecimalBD.multiply(new BigDecimal(60));
+            latMinutes = String.format(Locale.US,"%02d", latMinutesBD.toBigInteger());
+            var latMinutesDecimalBD = latMinutesBD.subtract(new BigDecimal(latMinutes));
+            latDecimalMinute = String.format(Locale.US,"%02d", latMinutesDecimalBD.multiply(new BigDecimal(100)).toBigInteger());
+
+            //deal with the long
+            var longBD = new BigDecimal(coordinate.getLongitude());
+            longDegrees = longBD.toBigInteger().toString();
+            var longDegreesDecimalBD = longBD.subtract(new BigDecimal(longDegrees));
+            var longMinutesBD = longDegreesDecimalBD.multiply(new BigDecimal(60));
+            longMinutes = String.format(Locale.US,"%02d", longMinutesBD.toBigInteger());
+            var longMinutesDecimalBD = longMinutesBD.subtract(new BigDecimal(longMinutes));
+            longDecimalMinute = String.format(Locale.US,"%02d", longMinutesDecimalBD.multiply(new BigDecimal(100)).toBigInteger());
+
+            String latitude = latDegrees+"."+latMinutes+"."+latDecimalMinute;
+            String longitude = longDegrees+"."+longMinutes+"."+longDecimalMinute;
+            finalCoords.add(new Coordinate(latitude, longitude));
+        }
+        return finalCoords;
+    }
+
+    private static ArrayList<Coordinate> convertViperCoords(ArrayList<Coordinate> decimalCoords){
         ArrayList<Coordinate> finalCoords = new ArrayList<>();
 
         for(Coordinate coordinate: decimalCoords){
