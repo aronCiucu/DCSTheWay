3eb1e59be33f5a2b75a8a8c3b175d11bf985c845
Aron <ardrone007@gmail.com>
2023-07-20 18:42:12 +0300

    Fixed user preference checkbox not working

--- src/utils/saveUserPreferences.js
+++ src/utils/saveUserPreferences.js
@@ -1,5 +1,8 @@
+import {uiActions} from "../store/ui";
+
 const { ipcRenderer } = window.require("electron");
 const saveUserPreferences = (newPreferences) => {
+    dispatch(uiActions.setUserPreferences(newPreferences));
     ipcRenderer.send("savePreferences", newPreferences);
 }
 
