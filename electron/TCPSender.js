df2460c81e269f5b6c89d790c12f4e75dc843227
Aron <ardrone007@gmail.com>
2024-02-07 12:20:45 +0200

    Fixed js error on TCP transmission

--- electron/TCPSender.js
+++ electron/TCPSender.js
@@ -1,12 +1,17 @@
 const net = require("net");
 const { ipcMain } = require("electron");
+
 class TCPSender {
   constructor() {
     ipcMain.on("messageToDcs", (event, msg) => {
       let client = new net.Socket();
-      client.connect(42070, "127.0.0.1", function () {
-        client.write(JSON.stringify(msg) + "\n");
-      });
+      client
+        .connect(42070, "127.0.0.1", function () {
+          client.write(JSON.stringify(msg) + "\n");
+        })
+        .on("error", (e) => {
+          console.log(e);
+        });
     });
   }
 }
